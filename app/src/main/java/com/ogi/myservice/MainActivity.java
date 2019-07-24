08fd2f1d6c0b84c675dfcd44147a1c7545649bc2
Ogi G Permana <hunterdumay@gmail.com>
2019-07-24 19:30:28 +0700

    Implementasi start dan stop Bound Service

--- app/src/main/java/com/ogi/myservice/MainActivity.java
+++ app/src/main/java/com/ogi/myservice/MainActivity.java
@@ -1,13 +1,20 @@
 package com.ogi.myservice;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     public Button btnStartService,  btnStartIntentService, btnStartBoundService, btnStopBoundService;
+    boolean mServiceBound = false;
+    BoundService mBoundService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mServiceBound){
+            unbindService(mServiceConnection);
+        }
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+           BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+           mBoundService = myBinder.getService();
+           mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+    };
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_start_service:
@@ -39,8 +68,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startService(mStartIntentService);
                 break;
             case R.id.btn_start_bound_service:
+                Intent mBoundServiceIntent = new Intent(MainActivity.this, BoundService.class);
+                bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
                 break;
             case R.id.btn_stop_bound_service:
+                unbindService(mServiceConnection);
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + v.getId());
