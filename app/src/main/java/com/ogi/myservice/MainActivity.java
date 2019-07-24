1fd1285e4b3256b00e79018e77f3f8705002fe9b
Ogi G Permana <hunterdumay@gmail.com>
2019-07-24 15:00:41 +0700

    Initial App

---
+++ app/src/main/java/com/ogi/myservice/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ogi.myservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnStartService,  btnStartIntentService, btnStartBoundService, btnStopBoundService;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStartService = findViewById(R.id.btn_start_service);
+        btnStartService.setOnClickListener(this);
+
+        btnStartIntentService = findViewById(R.id.btn_start_intent_service);
+        btnStartIntentService.setOnClickListener(this);
+
+        btnStartBoundService = findViewById(R.id.btn_start_bound_service);
+        btnStartBoundService.setOnClickListener(this);
+
+        btnStopBoundService = findViewById(R.id.btn_stop_bound_service);
+        btnStopBoundService.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_start_service:
+                Intent mStartService = new Intent(MainActivity.this, OriginService.class );
+                startActivity(mStartService);
+            break;
+            case R.id.btn_start_intent_service:
+                break;
+            case R.id.btn_start_bound_service:
+                break;
+            case R.id.btn_stop_bound_service:
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+    }
+}
